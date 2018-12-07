09594ec834aadc84080eb75b8bec2a48ae8a2c7f
khaarstad <kenneth@haarstad.info>
2018-12-07 11:28:30 +0100

    Created

---
+++ ARMTemplates/VirtualMachines/DeployVMTemplate.ps1
@@ -0,0 +1,6 @@
+﻿Connect-AzureRmAccount
+
+Get-AzureRmSubscription
+Select-AzureRmSubscription -SubscriptionName "Visual Studio Premium with MSDN"
+
+New-AzureRmResourceGroupDeployment -Name VmDeploy -ResourceGroupName TestRG -TemplateFile vm.json -TemplateParameterFile vm.parameters.json
